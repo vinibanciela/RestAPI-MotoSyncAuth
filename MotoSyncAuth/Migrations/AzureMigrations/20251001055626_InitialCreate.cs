a274eb6a2fe461f697a4d1c0b9992d8b054f3d38
vinibanciela <vinibanciela@gmail.com>
2025-10-01 02:56:39 -0300

    migrations do azure db

---
+++ MotoSyncAuth/Migrations/AzureMigrations/20251001055626_InitialCreate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MotoSyncAuth.Migrations.AzureMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AUDIT_LOG",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AUDIT_LOG", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ROLE",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ROLE", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "USUARIO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordResetToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordResetTokenExpiration = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USUARIO", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_USUARIO_ROLE_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "ROLE",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ROLE",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Administrador" },
+                    { 2, "Gerente" },
+                    { 3, "Funcionario" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "USUARIO",
+                columns: new[] { "Id", "Email", "PasswordHash", "PasswordResetToken", "PasswordResetTokenExpiration", "RoleId", "Username" },
+                values: new object[] { 1, "admin@motosync.com", "$2a$11$4nsFZ2KQaPd40Ri1xwSLXuvCnf4RtbfC2qIuweQmh/ByEKe80CvIy", null, null, 1, "admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_USUARIO_RoleId",
+                table: "USUARIO",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AUDIT_LOG");
+
+            migrationBuilder.DropTable(
+                name: "USUARIO");
+
+            migrationBuilder.DropTable(
+                name: "ROLE");
+        }
+    }
+}
